708e0db2eaf5ee24f09473edb1c835830b8b3df0
mbecca <mbecca@mecon.gov.ar>
2018-06-21 10:15:03 -0300

    Ajuste seguridad

--- pasae-oauth2-sso-roles-ui/src/main/java/ar/edu/unlp/pasae/pasaepractica/controller/UserController.java
+++ pasae-oauth2-sso-roles-ui/src/main/java/ar/edu/unlp/pasae/pasaepractica/controller/UserController.java
@@ -13,41 +13,47 @@ import org.springframework.web.bind.annotation.RestController;
 import ar.edu.unlp.pasae.pasaepractica.dto.UserDTO;
 
 @RestController
-@RequestMapping("/ui/user")
+@RequestMapping("/user")
 public class UserController {
 
-	@Autowired
-	private OAuth2RestTemplate restTemplate;
+  @Autowired
+  private OAuth2RestTemplate restTemplate;
 
-	@Value("${resource-server}/user")
-	private String userURL;
+  @Value("${resource-server}/user")
+  private String userURL;
 
-	private OAuth2RestTemplate getRestTemplate() {
-		return restTemplate;
-	}
+  private OAuth2RestTemplate getRestTemplate() {
+    return restTemplate;
+  }
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@GetMapping(value = "list")
-	public Collection<UserDTO> list() {
-		final ResponseEntity<Collection> forEntity = getRestTemplate().getForEntity(userURL + "/list",
-				Collection.class);
-		return forEntity.getBody();
-	}
+  @SuppressWarnings({
+      "rawtypes", "unchecked"
+  })
+  @GetMapping(value = "list")
+  public Collection<UserDTO> list() {
+    final ResponseEntity<Collection> forEntity = getRestTemplate().getForEntity(userURL + "/list",
+        Collection.class);
+    return forEntity.getBody();
+  }
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@GetMapping(value = "listForAdmin")
-	public Collection<UserDTO> listAdmin() {
-		final ResponseEntity<Collection> forEntity = getRestTemplate().getForEntity(userURL + "/listForAdmin",
-				Collection.class);
-		return forEntity.getBody();
-	}
+  @SuppressWarnings({
+      "rawtypes", "unchecked"
+  })
+  @GetMapping(value = "listForAdmin")
+  public Collection<UserDTO> listForAdmin() {
+    final ResponseEntity<Collection> forEntity = getRestTemplate().getForEntity(userURL + "/listForAdmin",
+        Collection.class);
+    return forEntity.getBody();
+  }
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@GetMapping(value = "listForStudent")
-	public Collection<UserDTO> listForStudent() {
-		final ResponseEntity<Collection> forEntity = getRestTemplate().getForEntity(userURL + "/listForStudent",
-				Collection.class);
-		return forEntity.getBody();
-	}
+  @SuppressWarnings({
+      "rawtypes", "unchecked"
+  })
+  @GetMapping(value = "listForStudent")
+  public Collection<UserDTO> listForStudent() {
+    final ResponseEntity<Collection> forEntity = getRestTemplate().getForEntity(userURL + "/listForStudent",
+        Collection.class);
+    return forEntity.getBody();
+  }
 
 }
